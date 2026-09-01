4b6b38633a5441d9d6fd06102323b86bad8188bd
HAFIZ MUHAMMAD RABEE SIDDIQUI <rabeesiddiqui09@gmail.com>
2026-09-01 05:21:49 -0700

    Add files via upload

---
+++ BasicRockPaperScissorGame.py
@@ -0,0 +1,10 @@
+import random
+userchoice=input("enter your choice:")
+choices=["rock","paper","scissor"]
+computerchoice=random.choice(choices)
+if(userchoice==computerchoice):
+    print(f"congratulations YOU WIN!")
+elif(userchoice not in choices):
+    print(f"plese enter eiter snake/water/gun")
+else:
+    print(f"computer win!")
