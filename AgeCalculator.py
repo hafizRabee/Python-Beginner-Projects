13910431b0f4384f042832a5927714dbf4ea6173
HAFIZ MUHAMMAD RABEE SIDDIQUI <rabeesiddiqui09@gmail.com>
2026-09-04 02:39:46 -0700

    Add files via upload

---
+++ AgeCalculator.py
@@ -0,0 +1,4 @@
+print("=======Age Calculator======")
+Birthyear=int(input("enter your birth year:"))
+currentyear=int(input("enter your current year:"))
+print(f"your age current age:{currentyear-Birthyear}")
