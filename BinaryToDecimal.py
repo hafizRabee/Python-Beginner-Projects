6d7411d39a9b6bafe85c294f45c4861b6ddd8e36
HAFIZ MUHAMMAD RABEE SIDDIQUI <rabeesiddiqui09@gmail.com>
2026-09-26 05:43:18 -0700

    Update BinaryToDecimal.py

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -1,15 +1,17 @@
 def BinaryToDecimal(n):
-  
-  result=0
-  for i in range(len(n),0,-1):
-    pow=len(n)-i
-    X=int(n[i-1])
-    result+=X*((2)**pow)
-  print(result)
+  if set(n) <= {"0", "1"}:
+    result=0
+    for i in range(len(n),0,-1):
+      pow=len(n)-i
+      X=int(n[i-1])
+      result+=X*((2)**pow)
+    print(result)
+  else:
+   print("binary only contain 0/1")
 
 try:
-  print("Note:This program works only with non-negative integers and does not accept negative or floating-point numbers.")
-  n=input("enter bnary:")
+  print("Note:This program accepts only non-negative binary integers containing 0s and 1s. Negative numbers and floating-point values are not supported.")
+  n=input("enter binary:")
   BinaryToDecimal(n)
 except ValueError:
   print("please enter Valid binary number")
